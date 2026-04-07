87f7ca8172cbe4c1938188d9af8e4189068a0628
Aditya Singh <adisingh1519@gmail.com>
2026-04-07 18:14:51 +0530

    Feat: Prepared schema for problems

---
+++ src/models/problem.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const problemSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: [true, "Difficulty is required"],
+    default: "easy",
+  },
+  testCases: [
+    {
+      input: {
+        type: String,
+        required: [true, "Input is required"],
+      },
+      output: {
+        type: String,
+        required: [true, "Output is required"],
+      },
+    },
+  ],
+  editorial: {
+    type: String,
+    required: [true, "Editorial is required"],
+  },
+});
+
+const Problem = mongoose.model("Problem", problemSchema);
+
+module.exports = Problem;
